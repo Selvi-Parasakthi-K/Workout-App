a7c70fffaaa986fa1fda3aacdb1c66344dd9886d
Selvi-Parasakthi-K <125342019+Selvi-Parasakthi-K@users.noreply.github.com>
2023-07-16 21:48:55 +0530

    Add files via upload

---
+++ backend/routes/workouts.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const {
+    getWorkouts,
+    getWorkout,
+    createWorkout,
+    deleteWorkout,
+    updateWorkout
+} = require('../controllers/workoutController')
+
+const router = express.Router()
+
+router.get('/', getWorkouts)
+
+// GET a single workout
+router.get('/:id', getWorkout)
+
+router.post('/', createWorkout)
+
+// DELETE a workout
+router.delete('/:id', deleteWorkout)
+
+// UPDATE a workout
+router.patch('/:id', updateWorkout)
+
+module.exports = router
